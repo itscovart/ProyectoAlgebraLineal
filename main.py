64b3fc79f6a0627138c9c0e7932d17b6dc240523
ItsCovart <covart06@gmail.com>
2025-10-29 23:39:08 -0600

    Implementacion del cors

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import matrices
 
 app = FastAPI(title="API para resolver matrices con Gauss-Jordan", version="1.0")
+origins = [
+    "http://localhost:5173",
+    "http://localhost", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       
+    allow_credentials=True,
+    allow_methods=["*"],        
+    allow_headers=["*"],        
+)
 
 app.include_router(matrices.router, prefix="/matrices", tags=["Matrices"])
 
