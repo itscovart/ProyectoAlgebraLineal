6e7156c934a28dfd50ce09959a40f8b8777ba976
ItsCovart <covart06@gmail.com>
2025-11-04 19:49:58 -0600

    Creacion de entidad de la base de datos

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS `railway`;
+USE `railway`;
+
+CREATE TABLE IF NOT EXISTS `Prueba` (
+  `id_prueba` INT NOT NULL AUTO_INCREMENT,
+  `operacion` VARCHAR(20) NOT NULL,
+  `resultado_es_valido` BOOLEAN NOT NULL,
+  `num_pasos` INT NOT NULL,
+  `matriz_inicial_json` JSON NOT NULL,
+  `comentario_resultado` TEXT NULL,
+  `fecha_registro` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_prueba`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+.env
+DB_HOST=turntable.proxy.rlwy.net
+DB_PORT=48281
+DB_USER=root
+DB_PASSWORD=ReCZmGuTKRKpLuOxSdgQABptzaviaVqG
+DB_NAME=railway
